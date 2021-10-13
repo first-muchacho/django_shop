e806ef358bb65adf02f7c97bc044280c8ba7d30c
SaVladimir <vova.savenko98@mail.ru>
2021-10-13 04:22:27 +0300

    Finished

--- djangoshop/djangoshop/settings.py
+++ djangoshop/djangoshop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^+$kuy6ppwzufx_m1xf*$v70=xts@emnx5_*ys=g8fy_6hg%g$'
+SECRET_KEY = 'Ваш ключ'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
